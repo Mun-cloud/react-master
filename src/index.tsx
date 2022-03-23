8af40a3bfae97c5a8b5828b2a146ad83da4dd81f
Taeho <mun05170@naver.com>
2022-03-23 22:48:51 +0900

    typescript 기초

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import { ThemeProvider } from "styled-components";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <ThemeProvider> */}
     <App />
+    {/* </ThemeProvider> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
