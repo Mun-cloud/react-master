16839757feed8f4d3512b8930ba3878c7cb6159d
Taeho <mun05170@naver.com>
2022-03-24 14:51:37 +0900

    타입스크립트 테마 적용 법

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import styled from "styled-components";
-import { keyframes } from "styled-components";
-import Circle from "./Circle";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
   return (
-    <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { lightTheme } from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <ThemeProvider> */}
-    <App />
-    {/* </ThemeProvider> */}
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
