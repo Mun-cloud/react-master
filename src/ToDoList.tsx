1806d6161cf1b50bb0506f648d9b1bc6fa4129c1
Taeho <mun05170@naver.com>
2022-04-07 23:39:45 +0900

    react-hook-form 1

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -22,14 +22,46 @@ import { useForm } from "react-hook-form";
 // };
 
 const ToDoList = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const onValid = (data: any) => {
     console.log(data);
   };
   return (
     <div>
-      <form onSubmit={handleSubmit(onValid)}>
-        <input {...register("Email")} placeholder="Email" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "please fill the field",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "네이버 이메일만 가능합니다.",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("Firstname", {
+            minLength: { value: 5, message: "5글자 이상 입력하시오" },
+          })}
+          placeholder="Firstname"
+        />
+        <input
+          {...register("Lastname", { required: true })}
+          placeholder="Lastname"
+        />
+        <input {...register("phone", { required: true })} placeholder="phone" />
+        <input
+          {...register("Nickname", { required: true })}
+          placeholder="Nickname"
+        />
         <button>Add</button>
       </form>
     </div>
