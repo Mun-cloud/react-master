3012e79362b0f6f46ffa700e13de8a44902ee9e7
Taeho <mun05170@naver.com>
2022-04-08 13:42:04 +0900

    react-hook-form 2

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,66 +1,30 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { atom } from "recoil";
 
-// const ToDoList = () => {
-//   const [toDo, setToDo] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     setToDo(event.currentTarget.value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log(toDo);
-//   };
+const toDoState = atom({
+  key: "toDo",
+  default: [],
+});
 
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-//         <button>Add</button>a
-//       </form>
-//     </div>
-//   );
-// };
+interface IForm {
+  toDo: string;
+}
 
 const ToDoList = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const onValid = (data: any) => {
-    console.log(data);
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = (data: IForm) => {
+    console.log("할 일 추가", data.toDo);
+    setValue("toDo", "");
   };
+
   return (
     <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
+      <form onSubmit={handleSubmit(handleValid)}>
         <input
-          {...register("email", {
-            required: "please fill the field",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "네이버 이메일만 가능합니다.",
-            },
+          {...register("toDo", {
+            required: "할 일을 적어주세요",
           })}
-          placeholder="Email"
-        />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("Firstname", {
-            minLength: { value: 5, message: "5글자 이상 입력하시오" },
-          })}
-          placeholder="Firstname"
-        />
-        <input
-          {...register("Lastname", { required: true })}
-          placeholder="Lastname"
-        />
-        <input {...register("phone", { required: true })} placeholder="phone" />
-        <input
-          {...register("Nickname", { required: true })}
-          placeholder="Nickname"
+          placeholder="Write a to do"
         />
         <button>Add</button>
       </form>
