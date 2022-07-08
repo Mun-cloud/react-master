203ce560b02b9e01b035da9f9d64601212a696b3
문태호(Mun TaeHo) <mun05170@naver.com>
2022-07-08 19:03:17 +0900

    toDo List 복습 시작

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,25 +1,80 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useRecoilValue } from "recoil";
-import { toDoSelector, toDoState } from "./atoms";
-import CreateToDo from "./components/CreateToDo";
-import ToDo from "./components/ToDo";
+// import { toDoSelector, toDoState } from "./atoms";
+// import CreateToDo from "./components/CreateToDo";
+// import ToDo from "./components/ToDo";
 
-const ToDoList = () => {
-  const [toDo, doing, done] = useRecoilValue(toDoSelector);
-  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    console.log();
+function ToDoList() {
+  const { register, handleSubmit, formState } = useForm(); // register함수가 props, change등 모든 것을 해줌
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
+  return (
+    <div>
+      {/** 두 개의 인자를 받음. (Valid, InValid) */}
+      <form
+        style={{ display: "flex", flexDirection: "column", width: "720px" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        {/** register함수가 반환하는 객체를 가져다가 input에 props로 줌 */}
+        {/** 요소에서 누군가 수정할 수 있기 때문에 required를 HTML에 넣는 것보다 JS에 넣는 것이 보안에 유리함. */}
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", {
+            required: "패스워드를 입력해주세요",
+            minLength: {
+              value: 5,
+              message: "패스워드가 너무 짧습니다.",
+            },
+          })}
+          placeholder="Password"
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+/* const ToDoList = () => {
+  const [toDo, setToDo] = useState("");
+  const [toDoError, setToDoError] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setToDoError("");
+    setToDo(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (toDo.length < 10) {
+      return setToDoError("10자 이상 입력해주세요");
+    }
+    console.log("submit");
   };
   return (
     <div>
-      <h1>To Dos</h1>
-      <hr />
-      <select onInput={onInput}>
-        <option value="TO_DO">To Do</option>
-        <option value="DOING">Doing</option>
-        <option value="DONE">Done</option>
-      </select>
-      <CreateToDo />
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} placeholder="write a todo" />
+        <button>Add</button>
+        {toDoError !== "" ? toDoError : null}
+      </form>
     </div>
   );
-};
+}; */
 
 export default ToDoList;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#0097e6",
-  textColor: "black",
-  accentColor: "#e84118",
+  bgColor: "#2f3640",
+  textColor: "white",
+  accentColor: "#9c88ff",
+  cardBgColor: "transparent",
 };
