747382b217f6ea167c848521d4c4ef89454c10f4
문태호(Mun TaeHo) <mun05170@naver.com>
2022-07-12 22:10:19 +0900

    state management 완료

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,14 +1,21 @@
 import { atom, selector } from "recoil";
 
+// 기본적으로 enum은 프로그래머를 도와주기 위해 일련의 숫자를 문자로 표현해준다.
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
 
-export const categoryState = atom({
+export const categoryState = atom<Categories>({
   key: "category",
-  default: "TO_DO",
+  default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDo[]>({
