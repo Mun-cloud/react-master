577bd6e53f3e779e0190ad1193b878925b17aa3b
문태호(Mun TaeHo) <mun05170@naver.com>
2022-07-13 20:19:02 +0900

    set selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { useRecoilState } from "recoil";
-import { minuteState } from "./atoms";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
+  // 첫번째 array의 값 : atom의 값
   const [minutes, setMinutes] = useRecoilState(minuteState);
+  // 첫번째 array의 값 : selector의 get 함수
+  const [hours, setHours] = useRecoilState(hourSelector);
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
-    event.preventDefault();
-    setMinutes(event.currentTarget.value);
+    setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
   return (
     <div>
-      <input value={minutes} type="number" placeholder="Minutes" />
-      <input type="number" placeholder="Hours" />
+      <input
+        onChange={onMinutesChange}
+        value={minutes}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        onChange={onHoursChange}
+        value={hours}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
