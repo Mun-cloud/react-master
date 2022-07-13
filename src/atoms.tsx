8575d6f3b68da494d4e74eae5431184a5d3371ff
문태호(Mun TaeHo) <mun05170@naver.com>
2022-07-13 19:01:00 +0900

    trello clone 시작

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,35 +1,6 @@
 import { atom, selector } from "recoil";
 
-// 기본적으로 enum은 프로그래머를 도와주기 위해 일련의 숫자를 문자로 표현해준다.
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
-});
-
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelecotr ",
-  // {get}이 있어야 atom의 데이터를 받을 수 있다.
-  // selector함수의 장점은 atom이 변경될 때 마다 연동된다는 것이다.
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
-  },
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
