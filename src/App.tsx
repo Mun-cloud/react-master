191f56822e0e0f4813f35f7ca083895c7c08b9a8
문태호(Mun TaeHo) <mun05170@naver.com>
2022-07-21 08:33:15 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import { useRef } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -7,6 +9,16 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
 `;
 const Box = styled(motion.div)`
   width: 200px;
@@ -18,21 +30,33 @@ const Box = styled(motion.div)`
 `;
 
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
+  hover: { scale: 1.1, rotateZ: 90 },
   click: { scale: 1, borderRadius: "100px" },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
-    <Wrapper>
-      <Box
-        drag
-        variants={boxVariants}
-        whileHover="hover"
-        whileDrag={{ backgroundColor: "rgb(46, 204, 113)" }}
-        whileTap="click"
-      ></Box>
-    </Wrapper>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route>
+          <Wrapper>
+            <BiggerBox ref={biggerBoxRef}>
+              <Box
+                drag
+                dragSnapToOrigin
+                dragElastic={1}
+                dragConstraints={biggerBoxRef}
+                variants={boxVariants}
+                whileHover="hover"
+                whileDrag={{ backgroundColor: "rgb(46, 204, 113)" }}
+                whileTap="click"
+              ></Box>
+            </BiggerBox>
+          </Wrapper>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
