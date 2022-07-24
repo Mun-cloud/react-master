abe0436d60a89c289531488c04a3babf65024316
문태호(Mun TaeHo) <mun05170@naver.com>
2022-07-24 21:05:56 +0900

    Revert "."
    This reverts commit f00545d9e1227025864e7faf24c25221ca5b36bd.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
-import { ThemeProvider } from "styled-components";
+// import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { theme } from "./theme";
+// import { darkTheme } from "./theme";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -78,10 +78,10 @@ a {
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      {/* <ThemeProvider theme={darkTheme}> */}
+      <GlobalStyle />
+      <App />
+      {/* </ThemeProvider> */}
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,15 +3,8 @@ import "styled-components";
 // and extend them!
 declare module "styled-components" {
   export interface DefaultTheme {
-    red: string,
-    black: {
-      veryDark: string,
-      darker:string,
-      lighter:string
-    },
-    white: {
-      lighter: string,
-      darker: string
-    }
+    bgColor: string;
+    boardColor:string;
+    cardColor:string;
   }
 }
